2d2e9706078eb3134ddee7c6d9a3d02b456e54ec
SandunUCSC <sandunchathuranga202@gmail.com>
2024-08-10 19:31:25 +0530

    navbar initiated

---
+++ react-restaurant-frontend/src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div className="logo">
+      <img src="/logo192.png" alt="logo" />
+      <h1>React Restaurant</h1>
+    </div>
+    
+  )
+}
+
+export default Logo
---
+++ react-restaurant-frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import Logo from "../components/Logo";
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="home-nav">
+        <Logo />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
