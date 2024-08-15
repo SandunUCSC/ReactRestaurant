b8ef431c03919ace7cfac973a7e5e69090b3ed7b
SandunUCSC <sandunchathuranga202@gmail.com>
2024-08-15 13:56:33 +0530

    navbar finished

--- react-restaurant-frontend/src/pages/Home.js
+++ react-restaurant-frontend/src/pages/Home.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "./Home.css";
 import Logo from "../components/Logo";
+import { Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+
+
 function Home() {
   return (
     <div className="home-container">
       <div className="home-nav">
         <Logo />
+<div>
+    <Link to="/Login">
+    <Button colorScheme='blue' variant="outline">Login</Button>
+    </Link>
+    <Link to="/Register">
+    <Button colorScheme='yellow' variant="outline">Register</Button>
+    </Link>
+
+
+</div>
+
+
+
       </div>
     </div>
   );
